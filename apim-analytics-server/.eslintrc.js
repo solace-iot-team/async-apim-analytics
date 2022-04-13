b175d7954c917e8181db379de58bb93411575bf6
Nils Eckert <nils.eckert@solace.com>
2022-04-13 08:59:01 +0200

    add eslint for code analysis

---
+++ apim-analytics-server/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: [
+    "server/@types/*"
+  ]
+};
