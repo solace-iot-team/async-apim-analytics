d9cae67309af030fe2e0032102194c35a0fd2819
Nils Eckert <nils.eckert@solace.com>
2022-05-09 15:12:53 +0200

    fix issue when test setup runs in github

--- apim-analytics-server/test/integration/setup.ts
+++ apim-analytics-server/test/integration/setup.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import s from 'shelljs';
+import fetch from 'node-fetch';
 import request from 'supertest';
 import server from '../../dist/server';
 import { logger } from '../lib/test-helper';
@@ -13,16 +14,44 @@ const connectorServerDirectory = `${resourcesDirectory}/apim-connector`;
 const dockerProjectName = 'amax-test';
 const dockerCompositeFile = `${connectorServerDirectory}/docker-compose.yml`
 
+const waitUntilConnectorIsAvailable = async (): Promise<void> => {
+
+  const serverPort = process.env.AMAX_SERVER_CONNECTOR_PORT ?? '8080';
+
+  const checkServer = async (): Promise<boolean> => {
+    return fetch(`http://localhost:${serverPort}`).then((response) => {
+      return (response.status == 200);
+    }, () => { return false; });
+  }
+
+  return new Promise(async (resolve) => {
+    while (!(await checkServer())) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+    resolve();
+  });
+}
+
 export async function mochaGlobalSetup() {
 
   logger.log(scriptName, 'Setup test environment ...');
 
-  logger.log(scriptName, 'Create containers for API Management Connector ...');
-  if (s.exec(`docker-compose -p ${dockerProjectName} -f "${dockerCompositeFile}" up -d --wait`).code != 0) {
+  // NOTE: github uses an older version of Docker Compose which doesn't support the '--wait'
+  //       parameter for the 'up' command.
+
+  logger.log(scriptName, 'Create docker containers for API Management Connector ...');
+  if (s.exec(`docker-compose -p ${dockerProjectName} -f "${dockerCompositeFile}" up -d`).code != 0) {
     logger.log(scriptName, 'ERROR: failed to create docker containers');
     process.exit(1);
   }
-  logger.log(scriptName, 'Containers for API Management Connector created');
+  logger.log(scriptName, 'Docker containers for API Management Connector created');
+
+  logger.log(scriptName, 'Wait until API Management Connector is up and running ...');
+  await waitUntilConnectorIsAvailable().catch(() => {
+    logger.log(scriptName, 'ERROR: connector is not available');
+    process.exit(1);
+  });
+  logger.log(scriptName, 'API Management Connector is up and running');
 
   // logger.log(scriptName, 'Create resources for API Management Connector ...');
   // await tools.createResources(`${resourcesDirectory}/test-organization.json`).catch(() => {
