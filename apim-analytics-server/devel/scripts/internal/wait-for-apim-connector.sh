4b504c0ee14f84d035321a567bacf49cddf220e2
Nils Eckert <nils.eckert@solace.com>
2022-05-16 15:21:18 +0200

    update setup for development environment

--- apim-analytics-server/devel/scripts/internal/wait-for-apim-connector.sh
+++ apim-analytics-server/devel/scripts/internal/wait-for-apim-connector.sh
@@ -3,23 +3,15 @@
 scriptDir=$(cd $(dirname "$0") && pwd);
 
 ############################################################################################################################
-# Settings
+# Prepare
 
-envFile="$scriptDir/../../.env"
-
-############################################################################################################################
-# Helper
-
-function getenv {
-  result=$(grep "${1}" "$envFile" -s | cut -f 2 -d '=')
-  echo ${result:-$2}
-}
+export $(grep -v '^#' "$scriptDir/../../.env" | xargs -0)
 
 ############################################################################################################################
 # Run
 
-printf "Wait until server is available "
-until curl "http://localhost:$(getenv AMAX_SERVER_CONNECTOR_PORT 8082)" -o /dev/null --silent; do
+printf "Wait until API Management Connector is available "
+until curl http://localhost:${AMAX_SERVER_CONNECTOR_PORT:-8082} -o /dev/null --silent; do
   printf "."
   sleep 1
 done
